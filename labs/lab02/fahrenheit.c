11db6c8e6765f0927ac8743de06b07cf1651dedf
Brian Chen <bchen17@cs.binghamton.edu>
2023-09-14 12:51:44 -0400

    DN

---
+++ labs/lab02/fahrenheit.c
@@ -0,0 +1,14 @@
+// Brian Chen
+#include <stdio.h>
+
+int main()
+{
+  printf("Type a temperature in fahrenheit: ");
+  float f;
+  scanf("%f", &f);
+  
+  printf("%f", (f-32.0)/1.8);
+    
+  printf("\nBrian Chen\n");
+  
+}
